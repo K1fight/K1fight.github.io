156335c91b49523d00e977f67ec343982ec05dee
K1fight <92151239+K1fight@users.noreply.github.com>
2024-01-01 12:07:13 -0500

    fixing 2

--- src/app/main.js
+++ src/app/main.js
@@ -3,6 +3,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './globals.css'
-
-
 createApp(App).mount('#app')
+
+//
+// import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+//
+// createApp({
+//     setup() {
+//     const message = ref(App)
+//     return {
+//         message
+// }
+// }
+// }).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/K1fight.github.io/",
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/K1fight.github.io/'
+      : '/',
   plugins: [
     vue(),
   ],
