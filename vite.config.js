bbe22a121ac24afdb71da34c075ccaf50cbddde2
K1fight <92151239+K1fight@users.noreply.github.com>
2024-01-01 17:02:10 -0500

    fixing 3

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'K1fight.github.io',
   publicPath: process.env.NODE_ENV === 'production'
       ? '/K1fight.github.io/'
       : '/',
